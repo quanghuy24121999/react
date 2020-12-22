9bc369496a8f7dbb22f71aad96da5ad24183935d
quanghuy24121999 <quanghuy24121999@gmail.com>
2020-12-22 14:27:58 +0700

    add badge quantity of products

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import ShoppingStackNavigator from './stackNavigator/ShoppingStackNavigator';
 import CartStack from './stackNavigator/CartStackNavigator';
 import OrdersStack from './stackNavigator/OrdersStackNavigator';
 import SettingStack from './stackNavigator/SettingStackNavigator';
-import { CartProvider } from './context/CartContext';
+import { CartProvider, CartContext } from './context/CartContext';
 
 Axios.defaults.baseURL = 'https://5fd8c0f47e05f000170d2f66.mockapi.io'
 
@@ -20,53 +20,57 @@ export default class App extends Component {
   render() {
     return (  
       <CartProvider>
-        <NavigationContainer>
-          <Tab.Navigator
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ focused, color, size }) => {
-                let iconName;
-
-                if (route.name === 'Home') {
-                  iconName = focused ? 'home' : 'home-outline';
-                } else if (route.name === 'Cart') {
-                  iconName = focused ? 'cart' : 'cart-outline';
-                } else if (route.name === 'Orders') {
-                  iconName = focused ? 'file-tray-stacked' : 'file-tray-stacked-outline';
-                } else if (route.name === 'Setting') {
-                  iconName = focused ? 'settings' : 'settings-outline';
-                }
-
-                return <Ionicons name={iconName} size={size} color={color} />;
-              }
-            })}
-
-            tabBarOptions={{
-              activeTintColor: '#026BF0',
-              inactiveTintColor: 'gray',
-            }}
-          >
-            <Tab.Screen
-              name="Home"
-              component={ShoppingStackNavigator}
-              options={{ title: 'Home' }}
-            />
-            <Tab.Screen 
-              name="Cart"
-              component={CartStack}
-              options={{ title: 'Cart' }} 
-            />
-            <Tab.Screen 
-              name="Orders"
-              component={OrdersStack} 
-              options={{ title: 'Orders' }}
-            />
-            <Tab.Screen 
-              name="Setting"
-              component={SettingStack} 
-              options={{ title: 'Setting' }}
-            />
-          </Tab.Navigator>
-        </NavigationContainer>
+        <CartContext.Consumer>
+          {({ quantity }) => (
+            <NavigationContainer>
+              <Tab.Navigator
+                screenOptions={({ route }) => ({
+                  tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+    
+                    if (route.name === 'Home') {
+                      iconName = focused ? 'home' : 'home-outline';
+                    } else if (route.name === 'Cart') {
+                      iconName = focused ? 'cart' : 'cart-outline';
+                    } else if (route.name === 'Orders') {
+                      iconName = focused ? 'file-tray-stacked' : 'file-tray-stacked-outline';
+                    } else if (route.name === 'Setting') {
+                      iconName = focused ? 'settings' : 'settings-outline';
+                    }
+    
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                  }
+                })}
+    
+                tabBarOptions={{
+                  activeTintColor: '#026BF0',
+                  inactiveTintColor: 'gray',
+                }}
+              >
+                <Tab.Screen
+                  name="Home"
+                  component={ShoppingStackNavigator}
+                  options={{ title: 'Home' }}
+                />
+                <Tab.Screen 
+                  name="Cart"
+                  component={CartStack}
+                  options={{ title: 'Cart', tabBarBadge: quantity}} 
+                />
+                <Tab.Screen 
+                  name="Orders"
+                  component={OrdersStack} 
+                  options={{ title: 'Orders' }}
+                />
+                <Tab.Screen 
+                  name="Setting"
+                  component={SettingStack} 
+                  options={{ title: 'Setting' }}
+                />
+              </Tab.Navigator>
+            </NavigationContainer>
+          )}
+        </CartContext.Consumer>
       </CartProvider>
     )
   };
